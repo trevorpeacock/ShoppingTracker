f71bfa064d629a793c93721b75bc8d41bf45d4ad
Trevor Peacock <trevorp@peacocktech.com>
2021-03-29 00:23:45 +1100

    only look up numberic barcodes

--- app.py
+++ app.py
@@ -11,49 +11,7 @@ from navigation import *
 from input import *
 from display import *
 from gui import TextApplication
-import openfoodfacts
-import requests
-import settings
-import hashlib
-import base64
-import hmac
-
-
-def lookup_item_online(stockitem):
-    print("Looking up", stockitem.barcode)
-    sig = base64.b64encode(
-        hmac.new(
-            settings.DIGITEYES_AUTH_KEY.encode('ascii'),
-            stockitem.barcode.encode('ascii'),
-            hashlib.sha1
-        ).digest()
-    ).decode('ascii')
-    search_result = requests.get(
-        'https://www.digit-eyes.com/gtin/v2_0/?upcCode={}&field_names=all&language=en&app_key={}&signature={}'.format(
-            stockitem.barcode, settings.DIGITEYES_APP_KEY, sig)).json()
-    print(search_result)
-    if search_result['return_code'] == '0':
-        if search_result['description']:
-            stockitem.name = search_result['description']
-        if search_result['brand']:
-            stockitem.brand = search_result['brand']
-        return True
-    search_result = openfoodfacts.products.get_product(stockitem.barcode)
-    print(search_result)
-    if search_result['status'] == 1:
-        stockitem.name = search_result['product']['product_name']
-        if 'product_name_en' in search_result['product']:
-            stockitem.name = search_result['product']['product_name_en']
-        if 'brands' in search_result['product']:
-            stockitem.brand = search_result['product']['brands']
-        return True
-    search_result = requests.get(
-        "https://eandata.com/feed/?v=3&keycode={}&mode=json&find={}".format(
-            settings.EANDATA_KEY, stockitem.barcode)).json()
-    print(search_result)
-    if search_result['status']['code'] == '200':
-        stockitem.name = search_result['product']['attributes']['product']
-        return True
+from lookup_barcode import lookup_item_online
 
 
 class Home(DisplayHandler):
